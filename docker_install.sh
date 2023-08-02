a1c6b4e37a853c2e05d0f79749d0d84b0aaed3c6
appar-sc <65882822+appar-sc@users.noreply.github.com>
2023-08-02 18:52:03 +0800

    feat: added docker install script

---
+++ docker_install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Script to install Docker and Docker Compose on Ubuntu 22.04
+
+# Update the apt package list
+sudo apt update -y
+
+# Install packages to allow apt to use a repository over HTTPS
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add Docker repository to APT sources
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Update the apt package list (again)
+sudo apt update -y
+
+# Install Docker
+sudo apt install -y docker-ce
+
+# Enable Docker to start on boot
+sudo systemctl enable docker
+
+# Add current user to Docker group to allow executing Docker commands without sudo
+sudo usermod -aG docker $USER
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/latest/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Set execute permission to the Docker Compose binary
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Print Docker and Docker Compose versions
+docker --version
+docker-compose --version
+
+echo "Docker and Docker Compose installation completed!"
