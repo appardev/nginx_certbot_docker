cf8fe44ecd9ee67bbe0fb0c9156062c454b3b7fe
appar-sc <65882822+appar-sc@users.noreply.github.com>
2023-08-23 14:25:20 +0800

    feat: added jenkins script final ok

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
+# Import Jenkins Key
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
+
 # Update System
 sudo apt update
 sudo apt upgrade -y
 
-# Install Dependencies
-sudo apt install software-properties-common apt-transport-https wget -y
-
 # Install Java
 sudo apt install openjdk-11-jdk -y
-
-# Add Jenkins Repository Key
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 
 # Add Jenkins Repository
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
 # Update Package List
-sudo apt install ca-certificates
 sudo apt update
 
 # Install Jenkins
@@ -30,4 +26,4 @@ sudo systemctl enable jenkins
 # Print the Initial Admin Password
 echo "Unlock Jenkins with the following password:"
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
-echo
+echo "Visit http://your_server_ip:8080 to complete the installation."
