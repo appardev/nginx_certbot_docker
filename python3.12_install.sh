8ba818f8c2dde9d7ee9d3028f58a9ef0be8eb6e1
appar-sc <65882822+appar-sc@users.noreply.github.com>
2025-02-12 16:31:16 +0800

    feat: python3.12 install

---
+++ python3.12_install.sh
@@ -0,0 +1,14 @@
+# python3.12
+sudo apt update
+sudo apt upgrade
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
+sudo apt install python3.12
+python3.12 --version
+
+# pip
+sudo apt install python3.12 python3.12-distutils
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.12
+
+# venv
+sudo apt-get install python3.12-dev python3.12-venv
